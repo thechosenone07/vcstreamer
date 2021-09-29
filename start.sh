738a0ab66de3d60875c5bcaa17cf7dd0d257c7d7
thechosenone07 <86468050+thechosenone07@users.noreply.github.com>
2021-09-29 13:10:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
-cd /VCPlayerBot
+git clone https://github.com/thechosenone07/vcstreamer
+cd /vcstreamer
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
